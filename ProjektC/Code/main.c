95d8568c7d9d313cd4069c9095334aeadf31d2de
HuLpeK <hubipl42@gmail.com>
2022-01-06 23:31:38 +0100

    Working get_user

--- ProjektC/Code/main.c
+++ ProjektC/Code/main.c
@@ -15,6 +15,7 @@
 
 int START_Header = 0;
 char* sciezka_zapisu;
+char uzytkownicy[256][256];
 void INIT(const char slowo[])
 {
     for(int i = 0; slowo[i] != '\0'; i++)
@@ -30,13 +31,29 @@ void INIT(const char slowo[])
     sciezka_zapisu[START_Header] = '\0';
 }
 
+void get_user(const char path[], char** user)
+{
+    char* help = (char*)calloc(strlen(path)-START_Header, sizeof(char));
+    for(int i = START_Header; path[i] != '\0'; i++)
+        help[i] = path[i];
+    *user = help;
+
+}
+
+
 int main(int argc, const char * argv[]) {
     
     INIT(argv[1]);
+    char* X;
+    get_user(argv[2], &X);
+    for(int i = 0; X[i] != '\0'; i++)
+        putchar(X[i]);
+    
+    exit(0);
     
     printf("Wybierz użytkownika z listy lub dodaj kolejnego:\n");
     
-    printf("[0]: <Dodaj Użytkownika>\n");
+    printf("[0]: <Dodaj Użytkownika>\n"); // Stworz To do select_menu
     for(int i = 1; i < argc; i++)
     {
         printf("[%d]: ", i);
@@ -49,12 +66,13 @@ int main(int argc, const char * argv[]) {
     int wybor;
     scanf("%d", &wybor);
     
-    if(wybor == 0)
+    if(wybor == 0) // SPRAWDZ czy jak dodajesz plik to taki nie istnieje!
     {
         printf("Podaj nazwe nowego użytkownika (Mozesz uzyc maksymalnie 256 znakow!)\n");
         char user[256];
         
         scanf("\n%[^\n]%*c", user);
+        
 			
         char user_path[strlen(sciezka_zapisu) + strlen(user)];
         strcpy(user_path,sciezka_zapisu);
@@ -68,7 +86,7 @@ int main(int argc, const char * argv[]) {
             printf("Blad Tworzenia Nowego Uzytkownika!!!");
             exit(0);
         }
-        
+        printf("%s", user_path);
             
     }
     
