48c1aeabd4e11defff999417ff2cc6a5230f1d1d
HuLpeK <hubipl42@gmail.com>
2022-01-06 13:33:01 +0100

    Add User List main.c

--- ProjektC/Code/main.c
+++ ProjektC/Code/main.c
@@ -6,9 +6,33 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int START_Header = 0;
+
+void INIT(const char slowo[])
+{
+    for(int i = 0; slowo[i] != '\0'; i++)
+        if(slowo[i] == '/')
+            START_Header = i+1;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    INIT(argv[1]);
+    printf("Wybierz użytkownika z listy lub dodaj kolejnego:\n");
+    
+    printf("0: <Dodaj Użytkownika>\n");
+    for(int i = 1; i < argc; i++)
+    {
+    
+        printf("%d: ", i);
+        for(int j = START_Header;  argv[i][j] != '\0'; j++)
+            putchar(argv[i][j]);
+        putchar('\n');
+        
+    }
+    
+    ///
     return 0;
 }
