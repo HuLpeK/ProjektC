46f7bf530370985c802b3c0f67788cd2b699dc12
HuLpeK <hubipl42@gmail.com>
2022-02-03 18:57:38 +0100

    working...

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir ./ProjektC/Saves
 
-#gcc -lm ./ProjektC/Code/main.c ./ProjektC/Code/Handlers/DateHandler.c ./ProjektC/Code/Handlers/DataHandler.c ./ProjektC/Code/Handlers/DataHandler.h ./ProjektC/Code/Handlers/CreateFileHandler.c ./ProjektC/Code/Handlers/Vector.c -o ./ProjektC/Code/main.o #HulpeK
+gcc -lm ./ProjektC/Code/main.c ./ProjektC/Code/Handlers/DateHandler.c ./ProjektC/Code/Handlers/DataHandler.c ./ProjektC/Code/Handlers/CreateFileHandler.c ./ProjektC/Code/Handlers/Vector.c -o ./ProjektC/Code/main.o #HulpeK
 
-​gcc -lm ./ProjektC/Code/main.c ./ProjektC/Code/Handlers/DateHandler.c ./ProjektC/Code/Handlers/DataHandler.c ./ProjektC/Code/Handlers/DataHandler.h ./ProjektC/Code/Handlers/CreateFileHandler.c ./ProjektC/Code/Handlers/Vector.c -o ./ProjektC/Code/main.o
+​#gcc -lm ./ProjektC/Code/main.c ./ProjektC/Code/Handlers/DateHandler.c ./ProjektC/Code/Handlers/DataHandler.c ./ProjektC/Code/Handlers/DataHandler.h ./ProjektC/Code/Handlers/CreateFileHandler.c ./ProjektC/Code/Handlers/Vector.c -o ./ProjektC/Code/main.o
 ​
 chmod +x ./ProjektC/Code/main.c
